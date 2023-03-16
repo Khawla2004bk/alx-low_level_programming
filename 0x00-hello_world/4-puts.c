db64879ca30daba33019e405bd45a7cbc265ab68
khawla <boukniter.khawla@gmail.com>
2023-03-16 05:08:32 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main : pricipal function
+ *
+ * return : Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
+}
