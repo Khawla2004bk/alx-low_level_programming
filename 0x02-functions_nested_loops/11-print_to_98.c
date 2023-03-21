2ac565028a034cf074a439c1c261bb38264d439e
khawla <boukniter.khawla@gmail.com>
2023-03-21 09:31:46 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: integer
+ */
+
+void print_to_98(int n)
+{
+	int counter;
+
+	if (n >= 98)
+		for (counter = n; counter >= 98; --counter)
+			printf("%d, ", counter);
+	else
+		for (counter = n; counter <= 98; ++counter)
+			printf("%d, ", counter);
+	printf("\n");
+}
