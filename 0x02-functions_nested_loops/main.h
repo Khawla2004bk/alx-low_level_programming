1d5a12050ab28260b0662933f4992348353f61df
khawla <boukniter.khawla@gmail.com>
2023-03-21 06:13:35 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,10 @@
 
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
 
 #endif /* MAIN_H */
