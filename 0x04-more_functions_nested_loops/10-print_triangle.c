045fb6b8f10e7d342dd1784ba7ad331da9729f1b
khawla <boukniter.khawla@gmail.com>
2023-03-23 09:31:29 -0700

    mandatory tasks

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,19 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= '0')
+	if (size <= 0)
 		_putchar('\n');
 	else
 	{
 		for (i = 1 ; i <= size ; i++)
 		{
-			for (j = i ; j <= size ; j++)
-				_putchar(' ');
-			for (j = 1 ; j<= i ; j++)
-				_putchar('#');
+			for (j = 1 ; j <= size ; j++)
+			{
+				if ((i + j) <= size)
+					_putchar(' ');
+				else 
+					_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
