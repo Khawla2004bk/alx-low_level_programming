790ad8b7d6d7840644412490a9b6f4dfd8dfc4d0
khawla <boukniter.khawla@gmail.com>
2023-04-03 10:38:09 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,15 +2,14 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: destination
- * @src: source
- * @n: bytes
- * Return: dest
+ * @n: function copies
+ * @src: bytes from memory area
+ * @dest: to memory area
+ * Return: a pointer to @dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,14 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, k = 0;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
-		for (j = 0 accept[j] != s[i] ; j++)
-			if (accept[j] == '\0')
-				return (i);
-	return (i);
+		if (s[i] != ' ')
+			for (j = 0 accept[j] != '\0' ; j++)
+				if (accept[j] == s[i])
+					z++;
+		else
+			return (k);
+	return (k);
 }
