41fa7b1e631b80417816284b1d9cb33ec94cf393
khawla <boukniter.khawla@gmail.com>
2023-04-05 12:50:30 -0700

    advanced

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,19 @@
 #include "main.h"
 
 /**
- * str_checker - compare two strings
- * @s1: string
- * @s2: string
- * @l: left index
- * @j: special index
- * Return: 1 if s palindrome, 0 otherwise
- */
-
-int str_checker(char *s1, char *s2, int l, int j)
-{
-	if (s1[l] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s1[l] == s2[j])
-		return (str_checker(s1, s2, l + 1, j + 1));
-	if (s1[l] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, l, j + 1));
-	if (s2[j] == '*')
-		return (str_checker(s1, s2, l + 1, j) || str_checker(s1, s2, l, j + 1));
-	return (0);
-/**
  * wildcmp - compares two strings
  * @s1: string
- * @s2: string
- * Return: 1 if identical strings, otherwise 0
+ * @s2: sting
+ * Return: 1 if identical, 0 otherwise
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
 }
