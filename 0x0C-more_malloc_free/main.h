d09e5db026dbfbca2823e106cc2f336166a6bf2d
khawla <boukniter.khawla@gmail.com>
2023-04-13 12:00:37 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,48 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+ * error_exit - prints error with _putchar
+ * Return: 98
+ */
+int error_exit(void)
+{
+	char *err;
+	int i;
+
+	err = "Error";
+	for (i = 0; err [i] != '\0'; i++)
+		_putchar(err[i]);
+	_putchar(err[i]);
+	exit(98);
+}
+/**
+ * check_number - cheks for num
+ * @str: string
+ * Return: 0 or 1
+ */
+int check_number(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+/**
+ * _length - length of str
+ * @str: string
+ * Rteurn: length of str
+ */
+int _length(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
 #endif
