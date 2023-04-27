fe0540b516b320bc510f690fb87a8680dd3f2790
khawla <boukniter.khawla@gmail.com>
2023-04-27 06:41:40 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,7 +26,8 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+int _putchar(char c);
+int _strlen(char *s);
 
 
 #endif
